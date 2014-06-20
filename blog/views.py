402d768a797d784c372de877fe77430ed7815bb8
Ilse Gerda Visser <ilsegv@pvv.ntnu.no>
2014-06-20 13:09:44 +0200

    using django-template with CSS

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader, Context
 
 # Create your views here.
-def index(request):
-	return render(request, 'blog/index.html')
+def blogindex(request):
+        print "rendering blog"
+	#return render(request, 'blog/index.html')
+        template = loader.get_template("blog/blog.html")
+        c = Context({"var": "Testing"})
+        return HttpResponse( template.render(c) )
